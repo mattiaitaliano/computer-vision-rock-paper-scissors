46b7071e93b24c5ab94ac2740a8b973a1a05cb65
mattiaitaliano <102301489+mattiaitaliano@users.noreply.github.com>
2022-12-01 19:01:29 +0000

    Add functions to play the game in manual_rps

--- manual_rps.py
+++ manual_rps.py
@@ -3,10 +3,38 @@ import random
 CHOICES = ["Rock", "Paper", "Scissors"]
 
 def get_computer_choice():
-    choice = random.choice(CHOICES)
-    return choice
+    return random.choice(CHOICES)
 
 
 def get_user_choice():
-    choice = input("Pick Rock, Paper or Scissor: ")
-    return choice
+    return input("Pick Rock, Paper or Scissor: ").capitalize()
+
+
+def get_winner(computer_choice, user_choice):
+    if computer_choice == user_choice:
+        return "It is a tie!"
+    else:
+        if computer_choice == CHOICES[0]:
+            if user_choice == CHOICES[1]:
+                return "You won!"
+            else:
+                return "You lost"
+        elif computer_choice == CHOICES[1]:
+            if user_choice == CHOICES[2]:
+                    return "You won!"
+            else:
+                    return "You lost"
+        else:
+            if user_choice == CHOICES[0]:
+                    return "You won!"
+            else:
+                    return "You lost"   
+
+
+def play():
+    computer_choice = get_computer_choice()
+    user_choice = get_user_choice()
+    print(get_winner(computer_choice, user_choice))
+    
+
+play()
