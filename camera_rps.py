a39be482a2335d1bec91ff0a5efa3bf73ec72b04
mattiaitaliano <102301489+mattiaitaliano@users.noreply.github.com>
2022-12-02 14:22:10 +0000

    Add time.time() function

--- camera_rps.py
+++ camera_rps.py
@@ -1,11 +1,14 @@
 import cv2
 from keras.models import load_model
 import numpy as np
+import time
+
 model = load_model('keras_model.h5')
 cap = cv2.VideoCapture(0)
 data = np.ndarray(shape=(1, 224, 224, 3), dtype=np.float32)
 
 def get_prediction(prediction):
+    time.time()
     if prediction[0][0] > 0.8:
         return "Rock"
     elif prediction[0][1] > 0.8:
